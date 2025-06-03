5cd29a84d500612d19def94bcc84de05b610aa8b
ScarletDawn03 <simenwei13@gmail.com>
2025-06-03 10:04:33 +0800

    CalanderAPI: Calander setup for Schedule Page

---
+++ src/services/calenderService.ts
@@ -0,0 +1,22 @@
+
+import axios from "../api/apiConfig";
+
+export interface CalendarEvent {
+  summary: string;
+  description: string;
+  start: string;  // ISO format
+  end: string;    // ISO format
+  email: string;  // Attendee email
+}
+
+export const createEvent = async (event: CalendarEvent): Promise<void> => {
+  await axios.post("/calendar/createEvent", event, { withCredentials: true });
+};
+
+export async function getEvents(userEmail: string) {
+  const res = await axios.get(`/calendar/getEvent`, {
+    params: { email: userEmail },
+    withCredentials: true,
+  });
+  return res.data;
+}
